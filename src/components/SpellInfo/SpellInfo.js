e906ff5267cd3b5f25926bad1dbaa78600c35b39
masonsteeger94 <masonsteeger@gmail.com>
2021-02-19 11:35:17 -0600

    more formatting to spell info

--- src/components/SpellInfo/SpellInfo.js
+++ src/components/SpellInfo/SpellInfo.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './SpellInfo.css'
 
 const SpellInfo = (props) => {
+    console.log(props.data)
     return(
         <div className='spell-info'>
             <div className='name-level'>
@@ -14,20 +15,26 @@ const SpellInfo = (props) => {
                 </div>
             </div>
             <div className='game-info'>
+                {props.data.concentration ? <div className='game-item'><h2>Concentration Required</h2></div> : null}
+                {props.data.ritual ? <div className='game-item'><h2>Ritual Required</h2></div> : null}
+                {props.data.damage ? <div className='game-item'><div><h2>Damage Type: {props.data.damage.damage_type.name}</h2></div> <div><h2>Damage At Slot Level:</h2>{Object.keys(props.data.damage.damage_at_slot_level).map((key, index)=> {return <p key={index}>Level {Object.keys(props.data.damage.damage_at_slot_level)[index]} :{props.data.damage.damage_at_slot_level[key]}</p>})}</div></div>: null}
+                {props.data.dc ? <div className='game-item'><h2>DC Type: {props.data.dc.dc_type.name}</h2> <h4>On success: {props.data.dc.dc_success === 'none' ? '0' : 'Half'} Damage</h4></div> : null}
                 <div className='game-item'>
                     <h2>Range: {props.data.range}</h2>
                 </div>
+                {props.data.area_of_effect ? <div className='game-item'><h2>AOE: {props.data.area_of_effect.size} ft. {props.data.area_of_effect.type}</h2></div> : null}
                 <div className='game-item'>
-                    <h2>School: {props.data.school.name}</h2>
+                    <h2>Casting Time: {props.data.casting_time}</h2>
+                </div>
+                <div className='game-item'>
+                    <h2>Duration: {props.data.duration}</h2>
                 </div>
                 <div className='game-item'>
                     <h2>Components: {props.data.components.join(', ')}</h2>
                 </div>
                 {props.data.material ? <div className='game-item'><h2>Materials Needed: {props.data.material}</h2></div> : null}
-                <div className='game-item'>
-                    <h2>Casting Time: {props.data.casting_time}</h2>
-                </div>
             </div>
+            <h3>School: {props.data.school.name}</h3>
             <h4>Description:</h4>
             {props.data.desc.map(text => {
                 return <p key={text}>{text}</p>
